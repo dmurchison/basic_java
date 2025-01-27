543d5b83582c810b4727f0b76924cb538d8b364d
dmurchison <duncancmurchison@gmail.com>
2025-01-27 10:04:02 -0500

    classes intro

---
+++ Java_Intro/_2_/classes/Restaurant.java
@@ -0,0 +1,18 @@
+package Java_Intro._2_.classes;
+
+public class Restaurant {
+    private String name;
+    private int rating;
+
+    public void setName(String restaurantName) { // Sets the restaurant's name
+        name = restaurantName;
+    }
+
+    public void setRating(int userRating) { // Sets the rating (1-5, with 5 best)
+        rating = userRating;
+    }
+
+    public void print() { // Prints name and rating on one line
+        System.out.println(name + " -- " + rating);
+    }
+}
