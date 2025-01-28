0464a7cfad5c604992fc2979bc2c3869bb6d272c
dmurchison <duncancmurchison@gmail.com>
2025-01-27 22:11:47 -0500

    MOD_3

---
+++ Java_Intro/_3_/YearChecker.java
@@ -0,0 +1,20 @@
+package Java_Intro._3_;
+
+import java.util.Scanner;
+
+public class YearChecker {
+    public static void main (String [] args) {
+        Scanner scnr = new Scanner(System.in);
+        int givenYear;
+
+        givenYear = scnr.nextInt();
+
+        if (givenYear < 1752) {
+            System.out.println("Not in the Gregorian calendar");
+        } else if ((givenYear % 4 == 0) && ((givenYear % 100 != 0) || (givenYear % 400 == 0))) {
+            System.out.println("Leap year");
+        } else {
+            System.out.println("Common year");
+        }
+    }
+}
