dd4607522fd71000d2248e3a6a5e8e5151954021
dmurchison <duncancmurchison@gmail.com>
2025-02-02 20:32:37 -0500

    enumerators n such

---
+++ Java_Intro/_3_/enums/TrafficLightControl.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+/* Manual controller for traffic light */
+public class TrafficLightControl {
+    // enum type declaration occurs outside the main method
+    public enum LightState {RED, GREEN, YELLOW, DONE}
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        LightState lightVal; // Declare variable of enum type
+        String userCmd;
+
+        lightVal = LightState.RED;
+        userCmd = "-";
+
+        System.out.println("User commands: n (next), r (red), q (quit).\n");
+
+        while (lightVal != LightState.DONE) {
+            if (lightVal == LightState.GREEN) {
+                System.out.print("Green light  ");
+                userCmd = scnr.next();
+                if (userCmd.equals("n")) { // Next
+                    lightVal = LightState.YELLOW;
+                }
+            } else if (lightVal == LightState.YELLOW) {
+                System.out.print("Yellow light  ");
+                userCmd = scnr.next();
+                if (userCmd.equals("n")) { // Next
+                    lightVal = LightState.RED;
+                }
+            } else if (lightVal == LightState.RED) {
+                System.out.print("Red light  ");
+                userCmd = scnr.next();
+
+                if (userCmd.equals("n")) { // Next
+                lightVal = LightState.GREEN;
+                }
+            }
+            if (userCmd.equals("r")) { // Force immediate red
+                lightVal = LightState.RED;
+            } else if (userCmd.equals("g")) { // Force immediate green
+                lightVal = LightState.GREEN;
+            } else if (userCmd.equals("y")) { // Force immediate yellow
+                lightVal = LightState.YELLOW;
+            } else if (userCmd.equals("q")) { // Quit
+                lightVal = LightState.DONE;
+            }
+        }
+
+        System.out.println("Quit program.");
+    }
+}
