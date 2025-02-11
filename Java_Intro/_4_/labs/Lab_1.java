07e9f633d35c880bc5d959c408b74ff01abfd2aa
dmurchison <duncancmurchison@gmail.com>
2025-02-11 12:59:09 -0500

    refactor Lab_1.java to improve weight input and output formatting

--- Java_Intro/_4_/labs/Lab_1.java
+++ Java_Intro/_4_/labs/Lab_1.java
@@ -8,16 +8,19 @@ public class Lab_1 {
         double[] weights = new double[DIFF_WEIGHTS];
         int i;
 
+        // Read weights
         for (i = 0; i < DIFF_WEIGHTS; ++i) {
             System.out.print("Enter weight " + (i + 1) + ": ");
             weights[i] = scnr.nextDouble(); // Read input as double
         }
+        System.out.print('\n');
 
-        System.out.println("You entered: ");
-        for (i = 0; i < DIFF_WEIGHTS - 1; ++i) {
+        // Print weights
+        System.out.print("You entered: ");
+        for (i = 0; i < DIFF_WEIGHTS; i++) {
             System.out.print(weights[i] + " ");
         }
-        System.out.println(weights[weights.length - 1]);
+        System.out.print('\n');
 
         // Total weight
         double totalWeight = 0;
@@ -25,9 +28,11 @@ public class Lab_1 {
             totalWeight += weights[i];
         }
         System.out.println("Total weight: " + totalWeight);
+
         // Average weight
         double averageWeight = totalWeight / DIFF_WEIGHTS;
         System.out.println("Average weight: " + averageWeight);
+
         // Maximum weight
         double maxWeight = weights[0];
         for (i = 1; i < DIFF_WEIGHTS; ++i) {
